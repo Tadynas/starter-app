a3f76d216ea911caf4cea69a7324b57019cc5698
Tadas Gricius <griciustadas@gmail.com>
2020-08-20 20:50:31 +0200

    Initial commit

---
+++ public/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.sass';
+
+ReactDOM.render(<p>Starter App</p>, document.getElementById('app'));
